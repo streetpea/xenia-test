67ba6b77b3e51d3822de90203927045b4f796ee9
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2025-01-18 20:52:06 -0300

    Update xenia_build.sh

--- xenia_build.sh
+++ xenia_build.sh
@@ -9,6 +9,7 @@ sudo apt autoremove
 wget https://apt.llvm.org/llvm.sh
 chmod +x llvm.sh
 sudo ./llvm.sh 18
+sudo apt install -y llvm-18 llvm-18-dev clang-18 lld-18 llvm-18-tools
 sudo ln -sf /usr/bin/clang-18 /usr/bin/clang
 sudo ln -sf /usr/bin/llvm-config-18 /usr/bin/llvm-config
 export AR=/usr/bin/ar

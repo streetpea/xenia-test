2c06309a21e681022954b942802e4e609faff7d3
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2025-01-18 18:49:52 -0300

    Update xenia_build.sh

--- xenia_build.sh
+++ xenia_build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 set -x
-sudo apt install -y premake premake5 lua desktop-file-utils build-essemtial clang make cmake libgtk-3-dev libpthread-stubs0-dev liblz4-dev libx11-dev libx11-xcb-dev libvulkan-dev libsdl2-dev libiberty-dev libunwind-dev libc++-dev libc++abi-dev ninja-build
+wget -c -q "https://github.com/premake/premake-core/releases/download/v5.0.0-beta4/premake-5.0.0-beta4-linux.tar.gz" ; tar xvf premake-5.0.0-beta4-linux.tar.gz ; chmod +x premake5 && sudo mv premake5 /usr/bin/
+sudo apt install -y lua5.4 desktop-file-utils build-essemtial clang make cmake libgtk-3-dev libpthread-stubs0-dev liblz4-dev libx11-dev libx11-xcb-dev libvulkan-dev libsdl2-dev libiberty-dev libunwind-dev libc++-dev libc++abi-dev ninja-build
 git submodule sync
 git clone https://github.com/xenia-project/xenia.git
 cd ./xenia/

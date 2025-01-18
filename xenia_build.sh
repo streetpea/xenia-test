3524a5ddfc95361f08a6ac5950b4c3b2c2776657
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2025-01-18 19:09:15 -0300

    Update xenia_build.sh

--- xenia_build.sh
+++ xenia_build.sh
@@ -2,9 +2,12 @@
 set -e
 set -x
 wget -c -q "https://github.com/premake/premake-core/releases/download/v5.0.0-beta4/premake-5.0.0-beta4-linux.tar.gz" ; tar xvf premake-5.0.0-beta4-linux.tar.gz ; chmod +x premake5 && sudo mv premake5 /usr/bin/
-sudo apt install -y lua5.4 desktop-file-utils build-essential clang make cmake libgtk-3-dev libpthread-stubs0-dev liblz4-dev libx11-dev libx11-xcb-dev libvulkan-dev libsdl2-dev libiberty-dev libc++-dev libc++abi-dev ninja-build
+sudo apt install -y lua5.4 desktop-file-utils build-essential clang make cmake libgtk-3-dev libpthread-stubs0-dev liblz4-dev libx11-dev libx11-xcb-dev libvulkan-dev libsdl2-dev libiberty-dev libc++-dev libc++abi-dev ninja-build python3-pip git ninja-build libvulkan-dev libxcb-keysyms1-dev libxkbcommon-dev libwayland-dev libx11-xcb-dev libxrandr-dev libgl-dev libxinerama-dev libxcursor-dev
 git submodule sync
 git clone https://github.com/xenia-project/xenia.git
 cd ./xenia/
 git submodule update --init --recursive --progress
-python3 xb build
+mkdir build/
+cd ./build/
+python3 ../xb premake --cc clang
+ninja

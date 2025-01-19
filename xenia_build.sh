596039c2244a2ec9ae3ed9069141aa9fb8e31bf0
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2025-01-18 23:45:14 -0300

    Update xenia_build.sh

--- xenia_build.sh
+++ xenia_build.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 mkdir lucas/
 wget -c -q "https://github.com/premake/premake-core/releases/download/v5.0.0-beta4/premake-5.0.0-beta4-linux.tar.gz" ; tar xvf premake-5.0.0-beta4-linux.tar.gz ; chmod +x premake5 ; sudo ln -rs premake5 /usr/bin/ ; sudo ln -rs premake5 -T /usr/bin/premake
-sudo apt install -y lua5.4 desktop-file-utils build-essential binutils make cmake libgtk-3-dev libpthread-stubs0-dev liblz4-dev libx11-dev libx11-xcb-dev libvulkan-dev libsdl2-dev libiberty-dev libc++-dev libc++abi-dev ninja-build python3-pip git ninja-build libvulkan-dev libxcb-keysyms1-dev libxkbcommon-dev libwayland-dev libx11-xcb-dev libxrandr-dev libgl-dev libxinerama-dev libxcursor-dev
+sudo apt install -y lua5.4 curl libcurl* desktop-file-utils build-essential binutils make cmake libgtk-3-dev libpthread-stubs0-dev liblz4-dev libx11-dev libx11-xcb-dev libvulkan-dev libsdl2-dev libiberty-dev libc++-dev libc++abi-dev ninja-build python3-pip git ninja-build libvulkan-dev libxcb-keysyms1-dev libxkbcommon-dev libwayland-dev libx11-xcb-dev libxrandr-dev libgl-dev libxinerama-dev libxcursor-dev
 sudo apt remove --purge llvm clang -y
 sudo apt autoremove
 wget https://apt.llvm.org/llvm.sh

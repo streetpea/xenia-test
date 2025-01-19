2861f3fc14d3e999495a9b0fe6c1c45b2e40a73b
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2025-01-18 23:22:54 -0300

    Update xenia_build.sh

--- xenia_build.sh
+++ xenia_build.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 mkdir lucas/
-wget -c -q "https://github.com/premake/premake-core/releases/download/v5.0.0-beta4/premake-5.0.0-beta4-linux.tar.gz" ; tar xvf premake-5.0.0-beta4-linux.tar.gz ; chmod +x premake5 && sudo mv premake5 /usr/bin/
+wget -c -q "https://github.com/premake/premake-core/releases/download/v5.0.0-beta4/premake-5.0.0-beta4-linux.tar.gz" ; tar xvf premake-5.0.0-beta4-linux.tar.gz ; chmod +x premake5 && sudo ln -rs premake5 /usr/bin/ && sudo ln -rs premake5 /usr/bin/premake
 sudo apt install -y lua5.4 desktop-file-utils build-essential binutils make cmake libgtk-3-dev libpthread-stubs0-dev liblz4-dev libx11-dev libx11-xcb-dev libvulkan-dev libsdl2-dev libiberty-dev libc++-dev libc++abi-dev ninja-build python3-pip git ninja-build libvulkan-dev libxcb-keysyms1-dev libxkbcommon-dev libwayland-dev libx11-xcb-dev libxrandr-dev libgl-dev libxinerama-dev libxcursor-dev
 sudo apt remove --purge llvm clang -y
 sudo apt autoremove

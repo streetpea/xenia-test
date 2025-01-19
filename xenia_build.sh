642f412b536f22c58aac4144999395e1c3b3924c
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2025-01-19 16:13:49 -0300

    Update xenia_build.sh

--- xenia_build.sh
+++ xenia_build.sh
@@ -3,9 +3,9 @@ echo $PATH
 set -e
 set -x
 mkdir lucas/
-sudo apt install -y findutils python3-opencv wget lua5.4 libgtk-3-dev liblz4-dev libglew-dev libx11-dev libsdl2-dev pkg-config curl libcurl4-gnutls-dev desktop-file-utils binutils make cmake libgtk-3-dev libpthread-stubs0-dev liblz4-dev libx11-dev libx11-xcb-dev libvulkan-dev libsdl2-dev libiberty-dev libc++-dev libc++abi-dev ninja-build python3-pip git ninja-build libvulkan-dev libxcb-keysyms1-dev libxkbcommon-dev libwayland-dev libx11-xcb-dev libxrandr-dev libgl-dev libxinerama-dev libxcursor-dev
+sudo apt install -y findutils libc6-dev build-essential python3-opencv wget lua5.4 libgtk-3-dev liblz4-dev libglew-dev libx11-dev libsdl2-dev pkg-config curl libcurl4-gnutls-dev desktop-file-utils binutils make cmake libgtk-3-dev libpthread-stubs0-dev liblz4-dev libx11-dev libx11-xcb-dev libvulkan-dev libsdl2-dev libiberty-dev libc++-dev libc++abi-dev ninja-build python3-pip git ninja-build libvulkan-dev libxcb-keysyms1-dev libxkbcommon-dev libwayland-dev libx11-xcb-dev libxrandr-dev libgl-dev libxinerama-dev libxcursor-dev
 #wget -c "https://github.com/premake/premake-core/releases/download/v5.0.0-beta4/premake-5.0.0-beta4-linux.tar.gz" ; tar xvf premake-5.0.0-beta4-linux.tar.gz ; chmod +x premake5 ; sudo cp premake5 /usr/bin/
-sudo apt remove --purge llvm clang gcc -y
+sudo apt remove --purge llvm clang -y
 sudo apt autoremove -y
 wget https://apt.llvm.org/llvm.sh
 chmod +x llvm.sh
@@ -15,10 +15,12 @@ sudo ln -sf /usr/bin/clang-18 /usr/bin/clang
 sudo ln -sf /usr/bin/llvm-config-18 /usr/bin/llvm-config
 export AR=llvm-ar-18
 export LD=gold
-export CXXFLAGS="$CXXFLAGS -v -Wno-integer-overflow -fuse-ld=lld -fno-lto"
+#export CXXFLAGS="$CXXFLAGS -v -Wno-integer-overflow -fuse-ld=lld -fno-lto"
+export CXXFLAGS="-lrt"
+export LDFLAGS="-lrt"
 export CXX=clang++-18
 export CC=clang-18
-export LDFLAGS="-fno-lto"
+#export LDFLAGS="-fno-lto"
 #git submodule sync
 git clone https://github.com/xenia-canary/xenia-canary.git
 cd ./xenia-canary/

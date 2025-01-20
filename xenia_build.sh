7177e0705a3fbba42751a72cf81105e59c440fc8
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2025-01-20 13:25:52 -0300

    Update xenia_build.sh

--- xenia_build.sh
+++ xenia_build.sh
@@ -29,12 +29,13 @@ git clone https://github.com/xenia-canary/xenia-canary.git
 cd ./xenia-canary/
 git submodule update --init --recursive --progress
 #make clean
-export CFLAGS=-O2
+#export CFLAGS=-O2
+#export CXXFLAGS="$CXXFLAGS -g0 -fno-lto"
 cp -f ${GITHUB_WORKSPACE}/main_init_posix.cc ./src/xenia/base/
 #git submodule deinit -f premake5
 #git rm --cached premake5
 #sed -i 's,"FatalWarnings",--"FatalWarnings",g' premake5.lua
-python3 xenia-build setup --target_os=linux && python3 xb premake --cc clang && python3 xb build --config=release
+CXXFLAGS="$CXXFLAGS -g0 -fno-lto" CFLAGS="-O2" python3 xenia-build setup --target_os=linux && python3 xb premake --cc clang && python3 xb build --config=release
 #mkdir build2/
 #cd ./build2/
 #make -v CXX=clang++-18 CC=clang-18 CXXFLAGS="-Wno-integer-overflow -fvar-tracking-assignments -fno-lto" LDFLAGS="-fvar-tracking-assignments -fno-lto" && make install --prefix=${GITHUB_WORKSPACE}/lucas/

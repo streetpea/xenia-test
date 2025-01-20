c2193b8b07c529a65740e9682b2260e6461c3f65
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2025-01-20 11:41:37 -0300

    Add files via upload

---
+++ main_init_posix.cc
@@ -0,0 +1,8 @@
+// main_init_posix.cc
+#include <iostream>
+
+// Função que simula a inicialização para POSIX (dummy function)
+extern "C" void main_init_posix() {
+    std::cout << "Inicializando POSIX (dummy)..." << std::endl;
+    // Adicione qualquer inicialização necessária aqui
+}

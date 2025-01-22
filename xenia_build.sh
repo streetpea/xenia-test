99e9faf6662a02d3fd76523585ca7e2282167615
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2025-01-21 23:34:07 -0300

    Update xenia_build.sh

--- xenia_build.sh
+++ xenia_build.sh
@@ -39,7 +39,7 @@ cp -f ${GITHUB_WORKSPACE}/main_init_posix.cc ./src/xenia/base/
 #sed -i 's,"FatalWarnings",--"FatalWarnings",g' premake5.lua
 python3 xenia-build setup --target_os=linux && python3 xb premake --cc gcc && python3 xb build --config=release
 cd ./lucas/
-xvfb-run -a -- find -iname ../build/ 'xenia' -type f | xarhs -i -t -exec ./lib4bin -p -v -r -e -s -k {} \
+xvfb-run -a -- find -iname ../build/ 'xenia' -type f | xargs -i -t -exec ./lib4bin -p -v -r -e -s -k {} \
   /usr/lib/libGLX* \
   /usr/lib/libstdc++* \
   /usr/lib/libEGL* \

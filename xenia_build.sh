6e7b273e77b5578aa01afdf618bb34c0e5d6ae54
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-01-23 04:16:25 +0000

    fix CI and refactor 2 (#2)
    * refactor and some fixes
    
    * fix CI
    
    * remove this since deps will be installed in the .yml
    
    * install all deps here
    
    * oops
    
    * Update xenia_build.sh
    
    * remove this

--- xenia_build.sh
+++ xenia_build.sh
@@ -4,54 +4,11 @@ set -eux
 
 LIB4BIN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
 APPIMAGETOOL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage"
+ICON="https://github.com/xenia-project/xenia/blob/master/assets/icon/256.png?raw=true"
 
 export APPIMAGE_EXTRACT_AND_RUN=1
 export ARCH="$(uname -m)"
-
-pacman -Syu --noconfirm linux-headers \
-	binutils \
-	patchelf \
-	findutils \
-	grep \
-	sed \
-	coreutils \
-	strace \
-	llvm \
-	cmake \
-	git \
-	pipewire \
-	pulseaudio \
-	pulseaudio-alsa \
-	findutils \
-	ninja \
-	base-devel \
-	gnupg \
-	lsb-release \
-	python-opencv \
-	wget \
-	lua \
-	gtk3 \
-	lz4 \
-	glew \
-	libx11 \
-	sdl2 \
-	pkgconf \
-	curl \
-	binutils \
-	vulkan-icd-loader \
-	vulkan-radeon \
-	vulkan-intel \
-	vulkan-nouveau \
-	vulkan-tools \
-	vulkan-headers \
-	xcb-util-keysyms \
-	xkeyboard-config \
-	wayland \
-	libxrandr \
-	mesa \
-	libxinerama \
-	libxcursor \
-	clang-tools-extra
+echo 'test' > ~/version
 
 git clone https://github.com/xenia-canary/xenia-canary.git
 cd ./xenia-canary/
@@ -64,24 +21,36 @@ wget "$APPIMAGETOOL" -O appimagetool
 chmod +x appimagetool
 
 cp -f ${GITHUB_WORKSPACE}/main_init_posix.cc ./src/xenia/base/
-python3 xenia-build setup --target_os=linux 
-python3 xb premake --cc gcc 
+python3 xenia-build setup --target_os=linux
+python3 xb premake --cc gcc
 python3 xb build --config=release
 
 cd ./lucas/
-mkdir bin/
+wget "$ICON" -O ./xenia.png
+echo '[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Xenia-canary
+Exec=xenia_canary
+Icon=xenia
+Categories=Game;Emulator;
+Terminal=false
+StartupNotify=false' > Xenia-canary.desktop
+
 find ../build/bin/Linux/Release/
-xvfb-run -a -- find ../build/bin/Linux/Release/ -executable -iname 'xenia*' -type f | xargs -i -t -exec ./lib4bin -p -v -r -e -s -k {} \
-  /usr/lib/libGLX* \
-  /usr/lib/libstdc++* \
-  /usr/lib/libEGL* \
+xvfb-run -a -- ./lib4bin -p -v -r -e -s -k \
+	../build/bin/Linux/Release/xenia* \
+	/usr/lib/libGLX* \
+	/usr/lib/libstdc++* \
+	/usr/lib/libEGL* \
 	/usr/lib/dri/* \
 	/usr/lib/libvulkan* \
 	/usr/lib/pipewire-0.3/* \
 	/usr/lib/spa-0.2/*/* \
 	/usr/lib/alsa-lib/*
- find ../build/ -executable -iname 'xenia*' -type f | xargs -i -t -exec cp -f {} ./bin/
- ln -frs ./bin/xenia_canary AppRun
- cp ${GITHUB_WORKSPACE}/Xenia-canary.desktop . ; cp ${GITHUB_WORKSPACE}/xenia.png .
+
+ln ./sharun ./AppRun
+./sharun -g
+
  cd ..
 ./appimagetool -n lucas/
